c6155b7a8d13d6e38927c8946c0ce700f7a46d96
1jack80 <work.davidmainoo@gmail.com>
2023-03-15 21:33:27 +0000

    refactor code structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import Button from "./button";
+import Button from "./components/button";
 
 function App() {
 	const [count, setCount] = useState(0);
 
-	return (
-		<div className="App">
-			<Button />
-		</div>
-	);
+	return <div className="App bg-gray-50"></div>;
 }
 
 export default App;
