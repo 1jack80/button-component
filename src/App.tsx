c6144d7c9f817f0b45aea54b3ddfc86cfa0d5b08
1jack80 <work.davidmainoo@gmail.com>
2023-03-15 23:36:22 +0000

    fix tailwind css integration

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Button from "./components/button";
+import Sidebar from "./components/sidebar";
 
 function App() {
 	const [count, setCount] = useState(0);
 
-	return <div className="App bg-gray-50"></div>;
+	return (
+		<div className="App bg-gray-50">
+			<Sidebar />
+		</div>
+	);
 }
 
 export default App;
